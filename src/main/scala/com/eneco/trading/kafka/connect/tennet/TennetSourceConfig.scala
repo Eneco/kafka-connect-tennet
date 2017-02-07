e792b213c9566dc446cee17e87351b842fa1cb31
Andrew Stevenson <andrew@datamountaineer.com>
2017-02-07 10:36:19 +0100

    adding extra topic configs

--- src/main/scala/com/eneco/trading/kafka/connect/tennet/TennetSourceConfig.scala
+++ src/main/scala/com/eneco/trading/kafka/connect/tennet/TennetSourceConfig.scala
@@ -10,8 +10,15 @@ class TennetSourceConfig(props: util.Map[String, String]) extends AbstractConfig
 
 object TennetSourceConfig {
 
-  val TOPIC = "tennet.topic"
-  val TOPIC_DOC = "The topic to write tennent data to."
+  val IMBALANCE_TOPIC = "tennet.imbalance.topic"
+  val IMBALANCE_TOPIC_DOC = "The topic to write imbalance tennet data to."
+
+  val SETTLEMENT_PRICE_TOPIC = "tennet.imbalance.topic"
+  val SETTLEMENT_PRICE_TOPIC_DOC = "The topic to write settlement price tennet data to."
+
+  val BID_LADDER_TOPIC = "tennet.bidladder.topic"
+  val BID_LADDER_DOC = "The topic to write bid ladder tennet data to."
+
   val URL = "tennet.url"
   val URL_DOC = "Tennet imbalance endpoint"
   val URL_DEFAULT = "http://www.tennet.org/xml/balancedeltaprices/balans-delta_2h.xml"
@@ -26,7 +33,9 @@ object TennetSourceConfig {
 
 
   val config: ConfigDef = new ConfigDef()
-    .define(TOPIC, Type.STRING,Importance.HIGH, TOPIC_DOC)
+    .define(IMBALANCE_TOPIC, Type.STRING,Importance.HIGH, IMBALANCE_TOPIC_DOC)
+    .define(SETTLEMENT_PRICE_TOPIC, Type.STRING,Importance.HIGH, SETTLEMENT_PRICE_TOPIC_DOC)
+    .define(BID_LADDER_TOPIC, Type.STRING,Importance.HIGH, BID_LADDER_DOC)
     .define(URL, Type.STRING, URL_DEFAULT, Importance.HIGH, URL_DOC)
     .define(REFRESH_RATE, Type.STRING, REFRESH_RATE_DEFAULT, Importance.LOW, REFRESH_RATE_DOC)
     .define(MAX_BACK_OFF, Type.STRING, MAX_BACK_OFF_DEFAULT , Importance.LOW, MAX_BACK_OFF_DOC)
